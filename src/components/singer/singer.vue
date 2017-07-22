3535e504e75dc26379b9fef3b7994d218e331680
nickylau82 <70458055@qq.com>
2017-07-23 00:13:44 +0800

    add singerList API

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -3,8 +3,33 @@
 </template>
 
 <script>
+	import {getSingerList} from 'api/singer';
+	import {ERR_OK} from 'api/config';
 	export default {
-		name: 'Singer'
+		name: 'Singer',
+		data () {
+			return {
+				singerList: []
+			};
+		},
+		mounted () {
+			this.$nextTick(() => {
+				this._getSingerList();
+			});
+		},
+		methods: {
+			_getSingerList () {
+				getSingerList()
+				.then(res => {
+					if (res.code === ERR_OK) {
+						this.singerList = res.data;
+					}
+				})
+				.catch(err => {
+					console.log(err);
+				});
+			}
+		}
 	};
 </script>
 
