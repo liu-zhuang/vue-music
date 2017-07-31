055ed66cb8ce8007d14217193dce06055e4ae0b5
nickylau82 <70458055@qq.com>
2017-07-31 23:54:29 +0800

    初始化用于播放页面的vux

--- src/store/mutation-types.js
+++ src/store/mutation-types.js
@@ -2,3 +2,8 @@
 // 理解为方法名的定义
 
 export const SET_SINGER = 'set_singer';
+export const SET_PLAYING = 'set_playing';
+export const SET_FULLSCREEN = 'set_fullscreen';
+export const SET_PLAYLIST = 'set_playlist';
+export const SET_SEQUENCEPLAYLIST = 'set_sequenceplaylist';
+export const SET_CURRENTINDEX = 'set_currentindex';
--- src/store/mutations.js
+++ src/store/mutations.js
@@ -5,5 +5,20 @@ import * as mutationTypes from './mutation-types';
 export default {
 	[mutationTypes.SET_SINGER] (state, singer) {
 		state.singer = singer;
+	},
+	[mutationTypes.SET_PLAYING] (state, playingState) {
+		state.playing = playingState;
+	},
+	[mutationTypes.SET_FULLSCREEN] (state, isFullScreen) {
+		state.fullScreen = isFullScreen;
+	},
+	[mutationTypes.SET_PLAYLIST] (state, playList) {
+		state.playList = playList;
+	},
+	[mutationTypes.SET_SEQUENCEPLAYLIST] (state, sequencePlayList) {
+		state.sequencePlayList = sequencePlayList;
+	},
+	[mutationTypes.SET_CURRENTINDEX] (state, currentIndex) {
+		state.currentIndex = currentIndex;
 	}
 };
